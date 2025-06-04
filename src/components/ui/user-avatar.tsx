0f20e3275b2b3a24362ae034126441bcf000d805
sahilkhude117 <sahilkhude11@gmail.com>
2025-06-04 13:06:46 +0530

    ui: shadcn

---
+++ src/components/ui/user-avatar.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+import { LogOut, SunMoon } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuItem } from '@/components/ui/dropdown-menu';
+
+export const UserAvatar = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[220px]">
+        <DropdownMenuLabel>User Email</DropdownMenuLabel>
+        <Link href="/settings/appearance">
+          <DropdownMenuItem className="cursor-pointer">
+            <SunMoon size={18} /> <span>Appearance</span>
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem className="cursor-pointer text-destructive">
+          <LogOut size={18} /> <span>Logout</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+
+
+
