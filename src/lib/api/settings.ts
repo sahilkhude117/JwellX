5424035b0dd88bbf0619883f660b606bc08deeac
sahilkhude117 <sahilkhude11@gmail.com>
2025-07-09 18:04:59 +0530

    apis: settings

---
+++ src/lib/api/settings.ts
@@ -0,0 +1,25 @@
+import { api } from "../api";
+import { 
+    ShopWithSettings,
+    UpdateShopRequest,
+    UpdateShopSettingsRequest,
+} from "../types/settings";
+
+export const settingsApi = {
+    getShop: () => 
+        api.get<ShopWithSettings>('/v1/shop'),
+
+    updateShop: (data: UpdateShopRequest) =>
+        api.patch<ShopWithSettings>('/v1/shop', data),
+
+    updateShopSettings: (data: UpdateShopSettingsRequest) =>
+        api.patch<ShopWithSettings>('/v1/shop/settings', data),
+
+    uploadLogo: (file: File) => {
+        const formData = new FormData();
+        formData.append('logo', file);
+        return api.post<{ logoUrl: string }>('/v1/shop/logo', formData, undefined, {
+            'Content-Type': 'multipart/form-data'
+        })
+    }
+}
