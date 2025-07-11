45187b93a128ee51dd4e6f3e09258f50422299f0
sahilkhude117 <sahilkhude11@gmail.com>
2025-07-11 17:22:25 +0530

    auth utils

--- src/lib/utils/tokens.ts
+++ src/lib/utils/tokens.ts
@@ -1,4 +1,6 @@
 import { randomBytes } from "crypto";
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
 
 export function generateVerificationToken(): string {
     return randomBytes(32).toString('hex');
@@ -6,4 +8,24 @@ export function generateVerificationToken(): string {
 
 export function generatePasswordResetToken(): string {
     return randomBytes(32).toString('hex')
+}
+
+export function generateRandomPassword(length: number = 12): string {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  let password = '';
+  
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  
+  return password;
+}
+
+export async function hashPassword(password: string): Promise<string> {
+  const saltRounds = 12;
+  return bcrypt.hash(password, saltRounds);
+}
+
+export async function verifyPassword(password: string, hash: string): Promise<boolean> {
+  return bcrypt.compare(password, hash);
 }
