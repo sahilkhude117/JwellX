11a671ca084088b9625b2a783e873ad6e72e8a85
sahilkhude117 <sahilkhude11@gmail.com>
2025-07-14 22:17:51 +0530

    api: settings

--- src/lib/api/settings/metal-rates.ts
+++ src/lib/api/settings/metal-rates.ts
@@ -1,9 +1,7 @@
 import { api } from "@/lib/api";
 import { 
     GrowwApiResponse,
-    HistoricalGoldData,
-    HistoricalSilverData,
-    MetalRatesFilters 
+    SilverData
 } from "@/lib/types/settings/metal-rates";
 
 export const metalRatesApi = {
@@ -11,13 +9,7 @@ export const metalRatesApi = {
     getCurrentGoldRates: () => 
         api.get<GrowwApiResponse>(`/v1/metal-rates/current-gold`),
 
-    // get historical gold data
-    getHistoricalGoldData: (city: string = 'delhi', days: number = 30) =>
-        api.get<HistoricalGoldData[]>(`/v1/metal-rates/historical-gold?city=${city}&days=${days}`),
+    getHistoricalSilverData: (city: string = 'delhi') => 
+        api.get<SilverData[]>(`/v1/metal-rates/historical-silver?city=${city}`),
 
-    getHistoricalSilverData: (city: string = 'delhi', days: number = 30) => 
-        api.get<HistoricalSilverData[]>(`/v1/metal-rates/historical-silver?city=${city}&days=${days}`),
-
-    getFilteredMetalRates: (filters: MetalRatesFilters) =>
-        api.get(`/v1/metal-rates/filtered`, { params: filters }),
 };
