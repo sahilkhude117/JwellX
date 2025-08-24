865f6d0c4e846cc80288adcea19bbb1a5454940e
sahilkhude117 <sahilkhude11@gmail.com>
2025-08-24 22:52:26 +0530

    ui: data table skeleton

---
+++ src/components/data-table/table-skeleton.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+interface TableSkeletonProps {
+  columns: number;
+  rows?: number;
+  showFilters?: boolean;
+  showBulkActions?: boolean;
+}
+
+export const TableSkeleton: React.FC<TableSkeletonProps> = ({
+  columns,
+  rows = 10,
+  showFilters = true,
+  showBulkActions = true,
+}) => {
+  return (
+    <div className="space-y-4">
+      {/* Filter Skeleton */}
+      {showFilters && (
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2">
+            <Skeleton className="h-10 w-64" />
+            <Skeleton className="h-10 w-32" />
+            <Skeleton className="h-10 w-32" />
+          </div>
+          <Skeleton className="h-10 w-24" />
+        </div>
+      )}
+
+      {/* Bulk Actions Skeleton */}
+      {showBulkActions && (
+        <div className="flex items-center space-x-2">
+          <Skeleton className="h-8 w-20" />
+          <Skeleton className="h-8 w-20" />
+        </div>
+      )}
+
+      {/* Table Skeleton */}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {Array.from({ length: columns }).map((_, i) => (
+                <TableHead key={i}>
+                  <Skeleton className="h-4 w-24" />
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: rows }).map((_, i) => (
+              <TableRow key={i}>
+                {Array.from({ length: columns }).map((_, j) => (
+                  <TableCell key={j}>
+                    <Skeleton className="h-4 w-20" />
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* Pagination Skeleton */}
+      <div className="flex items-center justify-between">
+        <Skeleton className="h-4 w-32" />
+        <div className="flex items-center space-x-2">
+          <Skeleton className="h-8 w-8" />
+          <Skeleton className="h-8 w-8" />
+          <Skeleton className="h-8 w-8" />
+          <Skeleton className="h-8 w-8" />
+        </div>
+      </div>
+    </div>
+  );
+};
